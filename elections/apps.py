b3ed469bb00d83be51d97ae815e8e9eb06ba81b1
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-02-20 05:23:00 +0000

    Configure elections app with custom AppConfig and verbose name

--- elections/apps.py
+++ elections/apps.py
@@ -1,6 +1,10 @@
+"""App configuration for the elections app."""
+
 from django.apps import AppConfig
 
 
 class ElectionsConfig(AppConfig):
+    """Configuration for the elections application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'elections'
+    verbose_name = 'Elections'
