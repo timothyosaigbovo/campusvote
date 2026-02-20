9ad3ff528a3c7b9c50442ba29b22737522f75519
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-02-20 06:18:39 +0000

    Configure management app with custom AppConfig and verbose name

--- management/apps.py
+++ management/apps.py
@@ -1,6 +1,10 @@
+"""App configuration for the management app."""
+
 from django.apps import AppConfig
 
 
 class ManagementConfig(AppConfig):
+    """Configuration for the management application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'management'
+    verbose_name = 'Election Management'
