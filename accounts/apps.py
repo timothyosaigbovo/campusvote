988addb5e2c6e7529f9f5ae69b08e880c2108edd
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-02-20 04:16:33 +0000

    Configure accounts app with custom AppConfig and verbose name

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,10 @@
+"""App configuration for the accounts app."""
+
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """Configuration for the accounts application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    verbose_name = 'User Accounts'
